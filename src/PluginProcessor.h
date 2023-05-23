7d535f1661f363276df523ac8e043a78508abd52
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2023-05-23 19:05:04 +0100

    Refactored low cut configuration

--- src/PluginProcessor.h
+++ src/PluginProcessor.h
@@ -68,7 +68,8 @@ private:
     using Filter = juce::dsp::IIR::Filter<float>;
     using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
     using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
-    
+    using Coefficients = Filter::CoefficientsPtr;
+
     MonoChain leftChain, rightChain;
 
     enum ChainPositions
@@ -79,8 +80,61 @@ private:
     };
 
     void updatePeakFilter(const ChainSettings &ChainSettings);
-    using Coefficients = Filter::CoefficientsPtr;
     static void updateCoefficients(Coefficients &old, const Coefficients &replacements);
+
+    template<typename ChainType, typename CoefficientType>
+    void updateCutFilter(ChainType& leftLowCut, const CoefficientType& cutCoefficients, 
+        const Slope& lowCutSlope)
+    {
+        leftLowCut.template setBypassed<0>(true);
+        leftLowCut.template setBypassed<1>(true);
+        leftLowCut.template setBypassed<2>(true);
+        leftLowCut.template setBypassed<3>(true);
+
+        switch (lowCutSlope)
+        {
+            case Slope_12:
+            {
+                leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+
+                break;
+            }
+            case Slope_24:
+            {
+                leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+                leftLowCut.template setBypassed<1>(false);
+
+                break;   
+            }
+            case Slope_36:
+            {
+                leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+                leftLowCut.template setBypassed<1>(false);
+                leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
+                leftLowCut.template setBypassed<2>(false);
+
+                break;   
+            }
+            case Slope_48:
+            {
+                leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+                leftLowCut.template setBypassed<1>(false);
+                leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
+                leftLowCut.template setBypassed<2>(false);
+                leftLowCut.template get<3>().coefficients = *cutCoefficients[3   ];
+                leftLowCut.template setBypassed<3>(false);
+                
+                break;   
+            }
+        }
+    }
 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessor)
