543690255da13e23045ae58d4d15358f81551ab5
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2023-05-24 11:28:49 +0100

    Added sliders to GUI

--- src/PluginProcessor.cpp
+++ src/PluginProcessor.cpp
@@ -171,8 +171,8 @@ bool AudioPluginAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* AudioPluginAudioProcessor::createEditor()
 {
-    // return new AudioPluginAudioProcessorEditor (*this);
-    return new juce::GenericAudioProcessorEditor(*this);
+    return new AudioPluginAudioProcessorEditor (*this);
+    // return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
